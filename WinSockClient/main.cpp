adb984cc8d9403c168f6a3db34cd0fb6d360c2c7
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-06-05 22:13:11 +0500

    Message from keyboard DONE

--- WinSockClient/main.cpp
+++ WinSockClient/main.cpp
@@ -61,42 +61,44 @@ void main()
 
 	//5) Получение и отправка данных:
 	//int recvbuflen = DEFAULT_BUFFER_LENGTH;
-	CONST CHAR SEND_BUFFER[] = "hello Server, Iam Client";
+	CHAR send_buffer[DEFAULT_BUFFER_LENGTH] = "hello Server, Iam Client";
 	CHAR recvbuffer[DEFAULT_BUFFER_LENGTH]{};
-
-	iResult = send(connect_socket, SEND_BUFFER, strlen(SEND_BUFFER), 0);
-	if (iResult == SOCKET_ERROR)
-	{
-		cout << "Send data failed with" << WSAGetLastError() << endl;
-		closesocket(connect_socket);
-		freeaddrinfo(result);
-		WSACleanup();
-		return;
-	}
-	cout << iResult << "Bytes sent" << endl;
-
-	//iResult = shutdown(connect_socket, SD_SEND);
-	if (iResult == SOCKET_ERROR)
-	{
-		cout << "Shutdown failed:" << WSAGetLastError() << endl;
-		closesocket(connect_socket);
-		freeaddrinfo(result);
-		WSACleanup();
-		return;
-	}
-	// 6)Receive data:
-
 	do
 	{
+		iResult = send(connect_socket, send_buffer, strlen(send_buffer), 0);
+		if (iResult == SOCKET_ERROR)
+		{
+			cout << "Send data failed with" << WSAGetLastError() << endl;
+			closesocket(connect_socket);
+			freeaddrinfo(result);
+			WSACleanup();
+			return;
+		}
+		cout << iResult << "Bytes sent" << endl;
+
+		//iResult = shutdown(connect_socket, SD_SEND);
+		/*if (iResult == SOCKET_ERROR)
+		{
+			cout << "Shutdown failed:" << WSAGetLastError() << endl;
+			closesocket(connect_socket);
+			freeaddrinfo(result);
+			WSACleanup();
+			return;
+		}*/
+		// 6)Receive data:
+
+
 		iResult = recv(connect_socket, recvbuffer, DEFAULT_BUFFER_LENGTH, 0);
-		if (iResult > 0) cout << "Bytes received:" << iResult << " Message:"<< recvbuffer << endl;
+		if (iResult > 0) cout << "Bytes received:" << iResult << " Message:" << recvbuffer << endl;
 		else if (iResult == 0) cout << "Connect closed" << endl;
 		else cout << "Receive failed with code: " << WSAGetLastError() << endl;
-		
+		ZeroMemory(send_buffer, sizeof(send_buffer));
+		ZeroMemory(recvbuffer, sizeof(recvbuffer));
+		cout << "ВВедите сообщение"; cin.getline(send_buffer, DEFAULT_BUFFER_LENGTH);
 	} while (iResult > 0);
 	//7) Disconnect:
 	iResult = shutdown(connect_socket, SD_SEND);
 	closesocket(connect_socket);
 	freeaddrinfo(result);
 	WSACleanup();
-}
+};
--- WinSockServer/main.cpp
+++ WinSockServer/main.cpp
@@ -91,13 +91,15 @@ void main()
 	SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
 	do
 	{
+		ZeroMemory(recvbuffer, sizeof(recvbuffer));
 		iResult = recv(ClientSocket,recvbuffer, recv_buffer_length,0);
 		if (iResult > 0)
 		{
 			cout << "Bytes received:" << iResult << endl;
 			cout << "Message:" << recvbuffer << endl;
 			CHAR sz_responce[]("Hello I am server");
-			INT iSendResult = send(ClientSocket, sz_responce, sizeof (sz_responce), 0);
+			//INT iSendResult = send(ClientSocket, sz_responce, sizeof (sz_responce), 0);
+			INT iSendResult = send(ClientSocket, recvbuffer, strlen (sz_responce), 0);
 			if (iSendResult == SOCKET_ERROR)
 			{
 				cout << "Error: Send failed with code:" << WSAGetLastError() << endl;
